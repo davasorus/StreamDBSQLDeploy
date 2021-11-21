b07ffa11220e4746a1a600570bb7eba69dea4dfb
Sean Davitt <sean.davitt@tylertech.com>
2021-11-21 14:31:13 -0500

    added powershell script to download and install .net 6.0 and start app

---
+++ RunMe.ps1
@@ -0,0 +1,5 @@
+﻿Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/a865ccae-2219-4184-bcd6-0178dc580589/ba452d37e8396b7a49a9adc0e1a07e87/windowsdesktop-runtime-6.0.0-win-x64.exe" -OutFile "C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe"
+
+ Start-Process -Wait -FilePath ‘C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe’ -ArgumentList ‘/install /quiet’
+
+ Start-Process -Wait -FilePath ‘StreamDBSQLDeploy.exe’
