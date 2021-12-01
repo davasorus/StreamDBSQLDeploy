33c7d51f88901821b6274003090575253f7793aa
Sean Davitt <sean.davitt@tylertech.com>
2021-12-01 12:21:55 -0500

    removed createShortCut powershell script and added to "Run Me"

--- RunMe.ps1
+++ RunMe.ps1
@@ -1,4 +1,7 @@
-﻿Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/a865ccae-2219-4184-bcd6-0178dc580589/ba452d37e8396b7a49a9adc0e1a07e87/windowsdesktop-runtime-6.0.0-win-x64.exe" -OutFile "C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe"
+﻿New-Item -Path "C:\Temp\" -ItemType Directory
+New-Item -Path "C:\Temp\Import Chat text" -ItemType Directory
+
+Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/a865ccae-2219-4184-bcd6-0178dc580589/ba452d37e8396b7a49a9adc0e1a07e87/windowsdesktop-runtime-6.0.0-win-x64.exe" -OutFile "C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe"
 
 Invoke-WebRequest -Uri "https://github.com/davasorus/ChatTextImporter/releases/download/1.0/ChatTextImport.exe" -OutFile "C:\Temp\Import Chat text\ChatTextImport.exe"
 
@@ -7,7 +10,16 @@ Start-Process -Wait -FilePath ‘C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.ex
 
 Start-Process -Wait -FilePath ‘StreamDBSQLDeploy.exe’
 
-Start-Process -Wait -FilePath ‘CreateShortCut.ps1’
+
+New-Item -Path "$env:USERPROFILE\desktop\Import Chat text" -ItemType Directory
+New-Item -Path "$env:USERPROFILE\desktop\Import Chat text" -Name "ChatTextImport Log.txt"
+
+$TargetFile = "C:\Temp\Import Chat text\ChatTextImport.exe"
+$ShortcutFile = "$env:USERPROFILE\desktop\Import Chat text\ChatTextImport.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
 
 $wsh = New-Object -ComObject Wscript.Shell
 
