5e6fb968ff5f779dd184cc5fa131c5d138380a5f
Sean Davitt <sean.davitt@tylertech.com>
2021-12-01 11:42:04 -0500

    added createShortCut powershell script to creates a folder on the desktop for the chat importer program

--- RunMe.ps1
+++ RunMe.ps1
@@ -1,13 +1,16 @@
 ﻿Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/a865ccae-2219-4184-bcd6-0178dc580589/ba452d37e8396b7a49a9adc0e1a07e87/windowsdesktop-runtime-6.0.0-win-x64.exe" -OutFile "C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe"
 
- Start-Process -Wait -FilePath ‘C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe’ -ArgumentList ‘/install /quiet’
+Start-Process -Wait -FilePath ‘C:\Temp\windowsdesktop-runtime-6.0.0-win-x64.exe’ -ArgumentList ‘/install /quiet’
 
- Start-Process -Wait -FilePath ‘StreamDBSQLDeploy.exe’
+Start-Process -Wait -FilePath ‘StreamDBSQLDeploy.exe’
 
- $wsh = New-Object -ComObject Wscript.Shell
+Start-Process -Wait -FilePath ‘CreateShortCut.ps1’
+
+$wsh = New-Object -ComObject Wscript.Shell
 
 $wsh.Popup("Computer Must Restart to finish. Once this is done your Database is deployed and ready for use")
 
+      
 Start-Sleep -Seconds 5
 
  Restart-Computer
